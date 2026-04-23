a7d7ff79897f7678258f32cb67597007caaea1e1
Jamil Ahmed <jamilahmedbd60@gmail.com>
2026-04-23 14:58:48 +0600

    fix: vite build chunk config for vercel

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,30 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+
   build: {
     outDir: 'dist',
     sourcemap: false,
     chunkSizeWarningLimit: 1600,
+
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          axios:  ['axios'],
-          socket: ['socket.io-client'],
-        },
-      },
-    },
+        manualChunks(id) {
+          if (!id.includes('node_modules')) return
+
+          if (id.includes('react')) return 'vendor'
+          if (id.includes('react-dom')) return 'vendor'
+          if (id.includes('react-router-dom')) return 'vendor'
+
+          if (id.includes('axios')) return 'axios'
+          if (id.includes('socket.io-client')) return 'socket'
+
+          return 'vendor'
+        }
+      }
+    }
   },
+
   server: {
     port: 5173,
   },
